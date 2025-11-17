63dd0ed155b132e29d3802b13ee41801a2c3caea
Brayan Mancera Tabarez <brayanbmt@hotmail.com>
2025-11-17 13:26:30 -0300

    fix scroll horizontal en CategoryTabs

--- lib/features/pos/presentation/widgets/category_tabs.dart
+++ lib/features/pos/presentation/widgets/category_tabs.dart
@@ -1,8 +1,9 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import '../../../../core/constants/app_colors.dart';
 import '../../../../core/constants/app_dimensions.dart';
 
-class CategoryTabs extends StatelessWidget {
+class CategoryTabs extends StatefulWidget {
   final List<String> categories;
   final String selectedCategory;
   final Function(String) onCategorySelected;
@@ -15,55 +16,102 @@ class CategoryTabs extends StatelessWidget {
   });
 
   @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: AppDimensions.categoryTabHeight + 10,
-      padding: const EdgeInsets.symmetric(vertical: AppDimensions.paddingS),
-      child: ListView.separated(
-        scrollDirection: Axis.horizontal,
-        padding: const EdgeInsets.symmetric(horizontal: AppDimensions.paddingM),
-        itemCount: categories.length,
-        separatorBuilder: (context, index) =>
-            const SizedBox(width: AppDimensions.paddingXS),
-        itemBuilder: (context, index) {
-          final category = categories[index];
-          final isSelected = category == selectedCategory;
+  State<CategoryTabs> createState() => _CategoryTabsState();
+}
 
-          return GestureDetector(
-            onTap: () => onCategorySelected(category),
-            child: Container(
-              height: AppDimensions.categoryTabHeight,
+class _CategoryTabsState extends State<CategoryTabs> {
+  late final ScrollController _scrollController;
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController = ScrollController();
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  static const _webScrollBehavior = WebScrollBehavior();
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: AppDimensions.categoryTabHeight + 10,
+      child: ScrollConfiguration(
+        behavior: _webScrollBehavior,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: AppDimensions.paddingS),
+          child: Scrollbar(
+            controller: _scrollController,
+            thumbVisibility: true,
+            child: ListView.separated(
+              controller: _scrollController,
+              scrollDirection: Axis.horizontal,
               padding: const EdgeInsets.symmetric(
-                horizontal: AppDimensions.paddingM,
-                vertical: AppDimensions.paddingS,
-              ),
-              decoration: BoxDecoration(
-                color: isSelected
-                    ? AppColors.categoryActive
-                    : AppColors.categoryInactive,
-                borderRadius:
-                    BorderRadius.circular(AppDimensions.borderRadiusL),
-                border: Border.all(
-                  color: isSelected
-                      ? AppColors.categoryActive
-                      : Colors.grey.shade300,
-                ),
-              ),
-              child: Center(
-                child: Text(
-                  category,
-                  style: Theme.of(context).textTheme.bodyMedium?.copyWith(
-                        color:
-                            isSelected ? Colors.white : AppColors.textPrimary,
-                        fontWeight:
-                            isSelected ? FontWeight.w600 : FontWeight.normal,
+                  horizontal: AppDimensions.paddingM),
+              physics: const BouncingScrollPhysics(),
+              itemCount: widget.categories.length,
+              separatorBuilder: (context, index) =>
+                  const SizedBox(width: AppDimensions.paddingXS),
+              itemBuilder: (context, index) {
+                final category = widget.categories[index];
+                final isSelected = category == widget.selectedCategory;
+
+                return GestureDetector(
+                  onTap: () => widget.onCategorySelected(category),
+                  child: Container(
+                    height: AppDimensions.categoryTabHeight,
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: AppDimensions.paddingM,
+                      vertical: AppDimensions.paddingS,
+                    ),
+                    decoration: BoxDecoration(
+                      color: isSelected
+                          ? AppColors.categoryActive
+                          : AppColors.categoryInactive,
+                      borderRadius:
+                          BorderRadius.circular(AppDimensions.borderRadiusL),
+                      border: Border.all(
+                        color: isSelected
+                            ? AppColors.categoryActive
+                            : Colors.grey.shade300,
                       ),
-                ),
-              ),
+                    ),
+                    child: Center(
+                      child: Text(
+                        category,
+                        style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+                              color: isSelected
+                                  ? Colors.white
+                                  : AppColors.textPrimary,
+                              fontWeight: isSelected
+                                  ? FontWeight.w600
+                                  : FontWeight.normal,
+                            ),
+                      ),
+                    ),
+                  ),
+                );
+              },
             ),
-          );
-        },
+          ),
+        ),
       ),
     );
   }
+}
+
+class WebScrollBehavior extends MaterialScrollBehavior {
+  const WebScrollBehavior();
+
+  @override
+  Set<PointerDeviceKind> get dragDevices => {
+        PointerDeviceKind.touch,
+        PointerDeviceKind.mouse,
+        PointerDeviceKind.stylus,
+        PointerDeviceKind.unknown,
+      };
 }
